5c7bd52f3c973f8bc84ef1eacc4237d00e87968b
Zac Mueterthies <zjm@iastate.edu>
2025-06-15 23:13:35 -0500

    cpu registers added

---
+++ 6502.c
@@ -0,0 +1,6 @@
+uint16_t PC     // Program Counter
+uint8_t SP      // Stack Pointer
+uint8_t A       // Accumulator
+uint8_t X       // Index Register X
+uint8_t Y       // Index Register Y
+uint8_t P       // Processor Status
